3fcd844de790c23d6c7abfe89dd41ab76c2ed81b
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-19 21:10:18 +0100

    dsfge

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+st.title('medical database')
