7b5430a19666d02cff5d3ba55332928c853f03a7
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-20 12:13:34 +0100

    test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import psycopg2
 
 hide_menu_style = """
         <style>
@@ -9,3 +9,27 @@ hide_menu_style = """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.title('medical database')
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from user;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
+
+
