ecceb39eee6ab74161755a9031792e3c68aac62c
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-21 09:56:24 +0100

    again

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def run_query(query):
 rows = run_query("SELECT * from public.user;")
 
 # Print results.
-st.write(rows)
-#for row in rows:
-#    st.write(f"User has a :{row[0]}:")
+
+for row in rows:
+    st.write(row)
 
 
