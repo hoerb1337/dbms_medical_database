e794a80c0a0c53ba6cf5c7a241f3f65e0c999001
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-21 09:53:13 +0100

    dbAccess

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from user;")
+rows = run_query("SELECT * from public.user;")
 
 # Print results.
 for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+    st.write(f"User has a :{row[0]}:")
 
 
