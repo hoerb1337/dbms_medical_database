bc0f70edee501f9b2da60421ac53a49deecc94b6
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-22 14:15:19 +0100

    sdfds

--- Home.py
+++ Home.py
@@ -5,34 +5,19 @@ from st_on_hover_tabs import on_hover_tabs
 ### Layout ###
 st.set_page_config(layout="wide")
 
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+#hide_menu_style = """
+ #       <style>
+ #       #MainMenu {visibility: hidden;}
+  #      </style>
+  #      """
+#st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.header("Custom tab component for on-hover navigation bar")
-#st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
 with st.sidebar:
-        tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
-                             iconName=['dashboard', 'money', 'economy'],
-                             styles = {'navtab': {'background-color':'#111',
-                                                  'color': '#818181',
-                                                  'font-size': '18px',
-                                                  'transition': '.3s',
-                                                  'white-space': 'nowrap',
-                                                  'text-transform': 'uppercase'},
-                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
-                                                                      'cursor': 'pointer'}},
-                                       'iconStyle':{'position':'fixed',
-                                                    'left':'7.5px',
-                                                    'text-align': 'left'},
-                                       'tabStyle' : {'list-style-type': 'none',
-                                                     'margin-bottom': '30px',
-                                                     'padding-left': '30px'}},
-                             key="1")
+    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+                         iconName=['dashboard', 'money', 'economy'], default_choice=0)
 
 if tabs =='Dashboard':
     st.title("Navigation Bar")
