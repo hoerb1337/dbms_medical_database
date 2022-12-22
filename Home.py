4ddd9a154ea53150b00912e7b89756eac86d58e5
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-22 15:55:35 +0100

    tabs

--- Home.py
+++ Home.py
@@ -14,27 +14,41 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.write("# Welcome to Medical Database! 👋")
 
-### database ###
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-conn = init_connection()
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    
+    ### database ###
 
-rows = run_query("SELECT * from public.user;")
+    # Initialize connection.
+    # Uses st.experimental_singleton to only run once.
+    @st.experimental_singleton
+    def init_connection():
+        return psycopg2.connect(**st.secrets["postgres"])
 
-# Print results.
+    conn = init_connection()
 
-for row in rows:
-    st.write(row)
+    # Perform query.
+    # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+    @st.experimental_memo(ttl=600)
+    def run_query(query):
+        with conn.cursor() as cur:
+            cur.execute(query)
+            return cur.fetchall()
+
+    rows = run_query("SELECT * from public.user;")
+
+    # Print results.
+
+    for row in rows:
+        st.write(row)
