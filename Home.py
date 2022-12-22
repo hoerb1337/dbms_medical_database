f1661deeadf9022bd922a99e96a1b320d1ace708
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-22 16:18:38 +0100

    tab4

--- Home.py
+++ Home.py
@@ -31,23 +31,27 @@ with tab3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     
+    ### database ###
+
     # Initialize connection.
     # Uses st.experimental_singleton to only run once.
     @st.experimental_singleton
     def init_connection():
         return psycopg2.connect(**st.secrets["postgres"])
-    
+
     conn = init_connection()
-    
+
     # Perform query.
     # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
     @st.experimental_memo(ttl=600)
     def run_query(query):
         with conn.cursor() as cur:
             cur.execute(query)
-        return cur.fetchall()
-    
+            return cur.fetchall()
+
     rows = run_query("SELECT * from public.user;")
+
     # Print results.
+
     for row in rows:
         st.write(row)
