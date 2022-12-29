c036fe01b953f023cb9e9a5fe68c22e1c0694c8f
hoerb1337 <eduard.kaucher@gmail.com>
2022-12-29 13:07:38 +0100

    reorganisation

---
+++ main.py
@@ -0,0 +1,52 @@
+# External libraries
+import streamlit as st
+# import psycopg2
+
+# Frontend modules
+import sideEffects
+#import analysis
+#import user
+#
+
+class Layout:
+    @st.experimental_memo(ttl=600)
+    def __init__(self):
+        
+        # Hide burger menu
+        hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+        st.markdown(hide_menu_style, unsafe_allow_html=True)
+        
+        # Webapp title
+        st.header("Medical Database")
+        
+        # Navigation bar
+        tab1, tab2, tab3 = st.tabs(["Medicine Side Effects", 
+                                    "Analysis",
+                                    "Your Access History"])
+        #
+
+        # Tab1: sideEffects
+        with tab1:
+            self.tab1_rendered = sideEffects.render_tab1()
+        #
+
+        # Tab2: analysis
+        with tab2:
+            #self.tab2_rendered = analysis.render_tab2()
+            st.header("Analysis")
+        #
+
+        # Tab3: user
+        with tab3:
+            #self.tab3_rendered = user.render_tab3()
+            st.header("Access history")
+        #
+
+
+if __name__ == "__main__":
+    rendered_layout = Layout()
+    
---
+++ sideEffects.py
@@ -0,0 +1,46 @@
+import streamlit as st
+#import psycopg2
+
+# Backend service
+#import sideEffectsService
+#import database
+
+class render_tab1:
+    def __init__(self):
+        st.write("Browse for side effects of selected medicines")
+
+        options = st.multiselect('Select up to two medicines:',
+                                ['Green', 'Yellow', 'Red', 'Blue'],
+                                ['Yellow', 'Red']
+                                )
+
+        st.write('You selected:', options)
+
+        agree = st.checkbox('I want side effects of combination')
+
+        if agree:
+            st.write('Great!')
+        
+        st.button(label="Lookup side effects")
+
+        #self.name = name
+        #self.age = age
+
+
+    # Perform query.
+    # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+    #@st.experimental_memo(ttl=600)
+    #def run_query(query):
+        #with conn.cursor() as cur:
+            #cur.execute(query)
+            #return cur.fetchall()
+
+    #rows = run_query("SELECT * from public.user;")
+
+    # Print results.
+
+    #for row in rows:
+        #st.write(row)
+
+if __name__ == "__main__":
+    pass
