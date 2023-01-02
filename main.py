64be8b27bde8772e7635ba8ac6f5b9d0b2bf2a79
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 21:24:49 +0100

    asd

--- main.py
+++ main.py
@@ -68,11 +68,11 @@ class Layout:
                     for key in st.session_state.keys():
                         del st.session_state[key]
                         st.write(st.session_state)
-                    #if st.button(label="Continue"):
+                    if st.button(label="Continue"):
                         #webbrowser.open("https://hoerb1337-dbms-medical-database-main-dev-93dds9.streamlit.app")
                     #st_autorefresh(interval=1000, limit=2, key="fizzbuzzcounter")
-                    st_autorefresh(interval=1000, limit=2, key="fizzbuzzcounter")
-                    st.experimental_rerun()
+                        st_autorefresh(interval=1000, limit=2, key="fizzbuzzcounter")
+                        st.experimental_rerun()
                     
         #
 
