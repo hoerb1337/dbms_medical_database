6e0de7ed0f18c543f24e2144e7b331803c46f039
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 20:29:25 +0100

    sadsad

--- main.py
+++ main.py
@@ -59,18 +59,18 @@ class Layout:
             # Post own side effects to database
                 if st.button(label="Report side effects", key="cont_reporting"):
                     tab1_rendered.report_side_effects(combo, nr_selected_meds, selected_meds, medicine1_side_effects, medicine2_side_effects)
+                    
                     st.success("Thank you! Your provided side effects have been successfully reported.")
-                    st.write("Click here to [continue](https://hoerb1337-dbms-medical-database-main-dev-93dds9.streamlit.app/)")
+                    #st.write("Click here to [continue](https://hoerb1337-dbms-medical-database-main-dev-93dds9.streamlit.app/)")
                     
                     if st.button(label="Continue"):
-                        if "ss_medicines_selected" in st.session_state:
-                            del ss_medicines_selected
+                        del st.session_state["medicines_selected"]
                         #for key in st.session_state.keys():
                             #del st.session_state[key]
                         #webbrowser.open("https://hoerb1337-dbms-medical-database-main-dev-93dds9.streamlit.app")
                         st_autorefresh(interval=1000, limit=2, key="fizzbuzzcounter")
                     
-                        st.experimental_rerun()
+                        #st.experimental_rerun()
         #
 
         # Tab2: analysis
