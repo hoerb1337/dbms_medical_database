1b381f47a4d696c404eb7e4e7e99eabdeb5fdcca
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 22:34:49 +0100

    sad

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class Layout:
                     
                     st.success("Thank you! Your provided side effects have been successfully reported.")
 
-                    n = 1000000000
+                    n = 100000000
                     while n >= 0:
                         n = n-1
                     st.experimental_rerun()
