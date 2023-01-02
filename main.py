6e3ab965598dccbf1c605db18fe3bbea8e54bb26
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 20:40:50 +0100

    sad

--- main.py
+++ main.py
@@ -64,11 +64,12 @@ class Layout:
                     #st.write("Click here to [continue](https://hoerb1337-dbms-medical-database-main-dev-93dds9.streamlit.app/)")
                     
                     if st.button(label="Continue"):
-                        del st.session_state["medicines_selected"]
-                        #for key in st.session_state.keys():
-                            #del st.session_state[key]
+                        #del st.session_state["medicines_selected"]
+                        for key in st.session_state.keys():
+                            del st.session_state[key]
+                        st.write(st.session_state)
                         #webbrowser.open("https://hoerb1337-dbms-medical-database-main-dev-93dds9.streamlit.app")
-                        st_autorefresh(interval=1000, limit=2, key="fizzbuzzcounter")
+                        #st_autorefresh(interval=1000, limit=2, key="fizzbuzzcounter")
                     
                         #st.experimental_rerun()
         #
