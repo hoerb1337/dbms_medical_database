bd6f2077844080fe174f2de1a0711339534161b9
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 14:32:51 +0100

    sadds

--- sideEffectsService.py
+++ sideEffectsService.py
@@ -159,6 +159,8 @@ class data4SideEffects:
             for side_effect_i in medicine1_side_effects:
                 # Exec query 
                 st.write(side_effect_i)
+                db = database.db_connection()
+                db_connection, db_cur = db.connect_postgres()
                 db_cur.execute("INSERT INTO dbms.mono_side_effects_reported(commercial_name, reported_by, reporting_date, individual_side_effect_name) VALUES ('commercial_name1', 100, '2002-02-02', 'individual_side_effect_name');")
         
         # Close connection
