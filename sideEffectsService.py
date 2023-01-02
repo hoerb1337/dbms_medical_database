e696ccd0df97b3407e4c8c1f4cf1a3102c4ed7be
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 14:49:52 +0100

    dsfdf

--- sideEffectsService.py
+++ sideEffectsService.py
@@ -158,7 +158,7 @@ class data4SideEffects:
             st.write("test")
             db = database.db_connection()
             db_connection, db_cur = db.connect_postgres()
-            db_cur.execute("""INSERT INTO dbms.mono_side_effects_reported(commercial_name, reported_by, reporting_date, individual_side_effect_name) VALUES (%(medname1)s, %(reported_by)s, %(date)s, %(medicine1_side_effects)s);""", {'medname1': selected_meds[0], 'reported_by': '99', 'date': '2002-02-02', 'medicine1_side_effects': 'test'})
+            db_cur.execute("""INSERT INTO dbms.mono_side_effects_reported(commercial_name, reported_by, reporting_date, individual_side_effect_name) VALUES (%(medname1)s, %(reported_by)s, %(date)s, %(medicine1_side_effects)s);""", {'medname1': 'adfddsdsf', 'reported_by': '99', 'date': '2002-02-02', 'medicine1_side_effects': 'test'})
             
             #db_cur.execute("""INSERT INTO dbms.mono_side_effects_reported (commercial_name, reported_by, reporting_date, individual_side_effect_name) VALUES (%s, %s, %s, %s);""", ('commercial_name1', 100, '2002-02-02', 'individual_side_effect_name'))
             #for side_effect_i in medicine1_side_effects:
