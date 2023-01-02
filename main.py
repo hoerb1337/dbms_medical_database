cfafe1b957cd66f13ed69b35f699afa39b4486e8
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 22:38:59 +0100

    sadsad

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class Layout:
                     
                     st.success("Thank you! Your provided side effects have been successfully reported. You will be automically forwarded to the selection of medicines...")
 
-                    n = 100000000
+                    n = 300000000
                     while n >= 0:
                         n = n-1
                     st.experimental_rerun()
