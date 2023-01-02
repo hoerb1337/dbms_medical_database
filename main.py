e3b169e6320003bfd068691760d4e4c1110fd9d8
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-02 13:54:37 +0100

    ddfgdsdwsf

--- main.py
+++ main.py
@@ -38,20 +38,20 @@ class Layout:
 
             # Start search for side effects
             if combo != None:
-                if st.button(label="Lookup side effects"):
+                if st.button(label="Lookup side effects", key="lookup"):
                     
                     # Show dataframe
                     tab1_rendered.display_sideEffects(nr_selected_meds, selected_meds, combo)
                     
                     # Reporting side effects
                     
-                    if st.button(label="Continue for reporting own side effects"):
+                    if st.button(label="Continue for reporting own side effects", key="cont_reporting"):
                             
                         # list of selected own side effects/multi select UI 
                         medicine1_side_effects, medicine2_side_effects = tab1_rendered.select_own_side_effects(combo, nr_selected_meds, selected_meds)
 
                         # Post own side effects to database
-                        if st.button(label="Report side effects"):
+                        if st.button(label="Report side effects", key="reporting"):
                             tab1_rendered.report_side_effects(combo, nr_selected_meds, selected_meds, medicine1_side_effects, medicine2_side_effects)
 
                     
