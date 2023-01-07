0b2dd2dc47ff8017f0812dd56321e5ae56282cf4
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-07 14:31:09 +0100

    hjhgdfgfd

--- analysis.py
+++ analysis.py
@@ -22,6 +22,7 @@ class render_tab2:
         if st.checkbox('I am taking two medicines in combination',
                        key="checkbox"):
             # Get list of side effects from combo medicines
+            st.experimental_rerun
             list_sideEffects = callAnalysisBackend.get_sideEffects(combo="True")
 
         else:
