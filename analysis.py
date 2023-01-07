cb1ef53508f71fc8a29edace19614b3f3d222ec0
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-07 11:42:22 +0100

    anal1

---
+++ analysis.py
@@ -0,0 +1,25 @@
+# External libraries
+import streamlit as st
+
+
+# Backend modules
+import analysisService
+#
+
+class render_tab2:
+    
+    def __init__(self):
+        st.info("Perform a reverse lookup:\n" +
+                "You report your own set of side effect symptoms, " +
+                "and we find for you the closest prediction of " +
+                "medicines that you could have taken.")
+
+    #def (self):
+        
+
+        #return 
+
+
+
+if __name__ == "__main__":
+    pass
