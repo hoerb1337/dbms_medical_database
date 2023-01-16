3609bd092bd1309e941fabbffeafa64249065850
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-16 10:00:26 +0100

    fdg

--- protein.py
+++ protein.py
@@ -23,8 +23,7 @@ class render_tab3:
             type: dataframe
         """
 
-        query = """
-                select to_char((avg(full_table.ratio_common_se))*100, 'fm900D00%') as avg_ratio_common_se
+        query = """select to_char((avg(full_table.ratio_common_se))*100, 'fm900D00%') as avg_ratio_common_se
                 from
                 (select gene_sideeffects.gene1 as gene, gene_sideeffects.se as side_effect,
                 gene_sideeffects.nr_shared_se as nr_common_se, shared_meds.nr_shared_meds as nr_shared_meds,
