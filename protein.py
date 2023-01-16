e692907c6aff282cd4e75f17785dbdfa85407e48
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-16 14:00:12 +0100

    d

--- protein.py
+++ protein.py
@@ -89,6 +89,7 @@ class render_tab3:
             # Runtime analysis execution
             protein_data = analysisService.data4Analysis()
             avg_ratio_se_meds, result_analysis = protein_data.lookup_avg_ratio_se_meds()
+            data_basis = protein_data.lookup_protein_se_meds()
             
             st.markdown("<br>", unsafe_allow_html=True)
             
@@ -106,7 +107,7 @@ class render_tab3:
             st.caption("More details to explore")
             
             query_total = "select to_char((avg(full_table.ratio_common_se))*100, 'fm900D00%') as avg_ratio_common_se from (select gene_sideeffects.gene1 as gene, gene_sideeffects.se as side_effect, gene_sideeffects.nr_shared_se as nr_common_se, shared_meds.nr_shared_meds as nr_shared_meds, gene_sideeffects.nr_shared_se::float/shared_meds.nr_shared_meds as ratio_common_se, to_char((gene_sideeffects.nr_shared_se::float/shared_meds.nr_shared_meds)*100, 'fm900D00%') as per_ratio_common_se from (select mp1.gene as gene1, mm.individual_side_effect as se, count(*) as nr_shared_se from dbms.medicine_protein mp1, dbms.medicine_mono mm where mp1.stitch = mm.stitch group by mp1.gene, mm.individual_side_effect)gene_sideeffects, (select mp.gene as gene2, count(*) as nr_shared_meds from dbms.medicine_protein mp group by gene)shared_meds where gene_sideeffects.gene1 = shared_meds.gene2 and shared_meds.nr_shared_meds > 1)full_table"
-            
+
             st.code(query_total, language="sql")
             st.markdown("<br>", unsafe_allow_html=True)
             st.warning("NOTICE: The execution of this analysis may take up to about 02:30mins, since it is done during runtime instead of accessing a final result.")
