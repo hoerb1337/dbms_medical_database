5f143ed2a854793689ebed7acfb4bb48a910de2c
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-16 10:01:51 +0100

    gfh

--- protein.py
+++ protein.py
@@ -23,25 +23,26 @@ class render_tab3:
             type: dataframe
         """
 
-        query = """select to_char((avg(full_table.ratio_common_se))*100, 'fm900D00%') as avg_ratio_common_se
-                from
-                (select gene_sideeffects.gene1 as gene, gene_sideeffects.se as side_effect,
-                gene_sideeffects.nr_shared_se as nr_common_se, shared_meds.nr_shared_meds as nr_shared_meds,
-                gene_sideeffects.nr_shared_se::float/shared_meds.nr_shared_meds as ratio_common_se,
-                to_char((gene_sideeffects.nr_shared_se::float/shared_meds.nr_shared_meds)*100, 'fm900D00%') as per_ratio_common_se
-                from
+        query = """
+select to_char((avg(full_table.ratio_common_se))*100, 'fm900D00%') as avg_ratio_common_se
+from
+(select gene_sideeffects.gene1 as gene, gene_sideeffects.se as side_effect,
+gene_sideeffects.nr_shared_se as nr_common_se, shared_meds.nr_shared_meds as nr_shared_meds,
+gene_sideeffects.nr_shared_se::float/shared_meds.nr_shared_meds as ratio_common_se,
+to_char((gene_sideeffects.nr_shared_se::float/shared_meds.nr_shared_meds)*100, 'fm900D00%') as per_ratio_common_se
+from
 
-                (select mp1.gene as gene1, mm.individual_side_effect as se, count(*) as nr_shared_se
-                from dbms.medicine_protein mp1, dbms.medicine_mono mm
-                where mp1.stitch = mm.stitch
-                group by mp1.gene, mm.individual_side_effect)gene_sideeffects,
+(select mp1.gene as gene1, mm.individual_side_effect as se, count(*) as nr_shared_se
+from dbms.medicine_protein mp1, dbms.medicine_mono mm
+where mp1.stitch = mm.stitch
+group by mp1.gene, mm.individual_side_effect)gene_sideeffects,
 
-                (select mp.gene as gene2, count(*) as nr_shared_meds
-                from dbms.medicine_protein mp
-                group by gene)shared_meds
+(select mp.gene as gene2, count(*) as nr_shared_meds
+from dbms.medicine_protein mp
+group by gene)shared_meds
 
-                where gene_sideeffects.gene1 = shared_meds.gene2
-                and shared_meds.nr_shared_meds > 1)full_table
+where gene_sideeffects.gene1 = shared_meds.gene2
+and shared_meds.nr_shared_meds > 1)full_table
                 """
         st.code(query, language="sql")
         
