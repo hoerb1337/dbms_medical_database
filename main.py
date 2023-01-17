c96bd17b193e47230d531d40f53caeff87dc85af
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-17 19:33:58 +0100

    sa

--- main.py
+++ main.py
@@ -22,9 +22,8 @@ class Frontend:
         #MainMenu {visibility: hidden;}
         </style>
         <h1>Welcome to the Medical Database</h1>
-        <h5>Our data is based on the <i>"Molecular network and polypharmacy data"</i>.
-        The data mainly provides information on medicines and their side effects.</h5>
-        <br>
+        <h6>Our data is based on the <i>"Molecular network and polypharmacy data"</i>.
+        The data mainly provides information on medicines and their side effects.</h6>
         <br>
         """
         st.markdown(header, unsafe_allow_html=True)
