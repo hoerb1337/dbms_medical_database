259d33aa68a04a5db5b0ac3653f522affcaa4478
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-18 21:51:44 +0100

    s

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class Frontend:
                         content: "";
                         position: absolute;
                         top: 100%;
-                        right: 5%;
+                        right: 40%;
                         margin-left: -30px;
                         border-width: 5px;
                         border-style: solid;
