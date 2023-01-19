b445e012e37d697652ded0615cf8bd367a964d7e
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-19 18:58:32 +0100

    f

--- analysisService.py
+++ analysisService.py
@@ -342,7 +342,7 @@ class data4Analysis:
                     max_p_total_calc_index = total_matched_sideEffects[0:index+1].index(max_p_total_calc)
                     sec_total_matched_sideEffects.pop(max_p_total_calc_index)
                     max_p_total_calc_2 = max(sec_total_matched_sideEffects)
-                    
+                    st.write(max_p_total_calc_2)
                     max_p_total_calc_index_2 = total_matched_sideEffects[0:index+1].index(max_p_total_calc_2)
                     max_p_total_2 = total_percent_matched_sideEffects[max_p_total_calc_index]
                     
