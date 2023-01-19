d877c9f7c7597b3ab87926b763cb6ec996e74b1a
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-19 15:10:09 +0100

    fd

--- analysisService.py
+++ analysisService.py
@@ -409,12 +409,13 @@ class data4Analysis:
                     
                     # end of list reached = all values same
                     elif index == len(count)-1:
+                        st.write(index)
                         break
                     
                     index = index + 1
 
                 st.write(index)
-                
+
                 # user reports
                 max_user_reported = "0.00%"
                 for i in p_user_reports[0:index]:
