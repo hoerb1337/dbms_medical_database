d9ae997a0f853d5afc79e6e7420f0feb1e1c4a0d
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-19 14:39:11 +0100

    sd

--- analysisService.py
+++ analysisService.py
@@ -482,7 +482,8 @@ class data4Analysis:
                         break
                     
                     # end of list reached = all values same
-                    elif index == len(count)+1:
+                    elif index == len(count)-1:
+                        index = len(count)-1
                         break
                     
                     index = index + 1
