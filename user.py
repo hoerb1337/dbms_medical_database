3adb555e9c3d1079990b6e26a78fb78f8e5d714e
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-21 21:39:16 +0100

    s

--- user.py
+++ user.py
@@ -1,7 +1,5 @@
 # External libraries
 import streamlit as st
-from datetime import datetime
-
 
 # Backend modules
 import userService
@@ -38,10 +36,10 @@ class render_tab4:
 
         get_user = userService.UserManagament()
         email, last_act = get_user.get_user_data_db(userID)
-        st.write(last_act)
-        date_string = '2021-12-31 15:37'
-        last_act = datetime.strptime(last_act, '%Y-%m-%d %H:%M:%S')
-        st.write(last_act)
+        
+        last_act1 = last_act[-6:-7:]
+        st.write(last_act1)
+
         info_box_tab4 = "<div class='info_box'><h5>Your Usage data:</h5><p>Welcome " + email + "! Your last acivity was on: " + last_act + ".</p><p>Browse for the history of all your actions on the Medical Database.</p></div>"
                     
         st.markdown(info_box_tab4, unsafe_allow_html=True)
