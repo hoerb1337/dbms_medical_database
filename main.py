8e53655da851b875fbda1889285dd64bcf966178
hoerb1337 <eduard.kaucher@gmail.com>
2023-01-27 19:37:49 +0100

    ProdRelease_v1.0

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Frontend:
                         }
                         .footer {
                             visibility: visible;
-                            position: fixed;
+                            position: relative;
                             left: 0;
                             bottom: 0;
                             width: 100%;
